1e086947374b041d895406d6b1756fa20f0d646d
SurenVithanage2 <it21203244@my.sliit.lk>
2023-05-15 11:44:07 +0530

    Update_Employee

---
+++ backend/server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+require('dotenv').config()
+
+const mongoose = require('mongoose')
+
+const employeeRoutes = require('./routes/employee')
+const employeeattendRoutes = require('./routes/employeeattend')
+const employeesalaryRoutes = require('./routes/employeesalary')
+
+
+//express app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req,res, next) =>{
+    console.log(req.path, req.method)
+    next()
+})
+
+
+//routes
+app.use('/api/employee', employeeRoutes)
+app.use('/api/employeeattend', employeeattendRoutes)
+app.use('/api/employeesalary', employeesalaryRoutes)
+
+
+
+//connect to db
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        //listening for requests
+        app.listen(process.env.PORT, ()=> {
+            console.log('connected to db & listening on port', process.env.PORT)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
+
---
+++ frontend/src/App.js
@@ -0,0 +1,72 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+//pages & components
+
+
+import EmployeeProfile from './pages/EmployeeProfile';
+import EmployeeRegister from './components/EmployeeRegister';
+import SalaryCalculator from './components/SalaryCalculator';
+import EmployeeS from './pages/EmployeeS';
+import EmployeeUpdate from './components/EmployeeUpdate';
+import EmployeeAttendance from './pages/EmployeeAttendance';
+import EmployeeAttView from './pages/EmployeeAttView';
+import EmployeeNavbar from './components/EmployeeNavbar';
+import EmployeeHome from './pages/EmployeeHome';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+        <EmployeeNavbar/>
+        
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/"
+              element={<EmployeeHome/>}
+            />
+
+          <Route
+              path="/EmployeeProfile"
+              element={<EmployeeProfile/>}
+            />
+
+            <Route
+              path="/EmployeeRegister"
+              element={<EmployeeRegister/>}
+            />
+
+              <Route
+              path="/SalaryCalculator"
+              element={<SalaryCalculator/>}
+            />
+
+            <Route
+              path="/EmployeeS"
+              element={<EmployeeS/>}
+            />
+
+            <Route
+              path="/update/:id"
+              element={<EmployeeUpdate />}
+            />
+
+            <Route
+              path="/EmployeeAttendance"
+              element={<EmployeeAttendance/>}
+            />
+
+            <Route
+              path="/EmployeeAttView"
+              element={<EmployeeAttView/>}
+            />
+
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
